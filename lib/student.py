51ca4d0087db3471b501399ed3df4f2c06ec08f1
Yvonne <ys359375@gmail.com>
2025-05-21 13:14:07 +0300

    .

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,16 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name, knowledge=[]):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, info):
+        self.info = info
+        self.knowledge.append(self.info)
+
+student1 = Student('Filla', "Amedi")
+student1.learn('Python')
+student2 = Student('rilla', "Amedi")
+student2.learn('Javascript')
+print(student2.knowledge)
